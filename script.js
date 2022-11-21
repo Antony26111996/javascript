64b6032c0996bcc4f99433acf57d506f224c0c00
Antony Raj <114063448+Antony26111996@users.noreply.github.com>
2022-11-21 17:46:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+function add(){
+    var x = document.getElementById("one").value;
+    var y = document.getElementById("two").value;
+    var z = parseInt(x)+parseInt(y);
+    alert(z);
+}
+function sub(){
+    var x = document.getElementById("one").value;
+    var y = document.getElementById("two").value;
+    var z = parseInt(x)-parseInt(y);
+    alert(z);
+}
+function multiply(){
+    var x = document.getElementById("one").value;
+    var y = document.getElementById("two").value;
+    var z = parseInt(x)*parseInt(y);
+    alert(z);
+}
+function div(){
+    var x = document.getElementById("one").value;
+    var y = document.getElementById("two").value;
+    var z = parseInt(x)/parseInt(y);
+    alert(z);
+}
